31316427b8e83a88f93bd25ffb1ba80b15a0c290
K-ost <golubkovru@gmail.com>
2024-10-01 14:41:08 +0300

    move cors higher

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8000;
 const server = jsonServer.create();
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
+server.use(cors());
 
 // Add custom routes
 server.get("/api/budgets/transactions", async (req, res) => {
@@ -28,35 +29,7 @@ server.get("/api/budgets/transactions", async (req, res) => {
   }
 });
 
-// server.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://personal-finance-server.onrender.com"
-//   );
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
 server.use(middlewares);
-server.use(cors({ origin: "http://localhost:5173" }));
 server.use("/api", auth);
 server.use("/api", router);
 
