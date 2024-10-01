d5e29bffbfa8fce76cddb211a329d0bbb228f313
K-ost <golubkovru@gmail.com>
2024-10-01 14:27:56 +0300

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const db = require("./db.json");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
 
@@ -9,21 +10,26 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 // Add custom routes
-server.get("/api/budgets/transactions", function (req, res) {
-  const output = db.budgets.map((budget) => {
-    const transactions = db.transactions
-      .filter((el) => el.category === budget.category && el.amount < 0)
-      .slice(0, 3);
-    return {
-      ...budget,
-      spent: 0,
-      latest: transactions,
-    };
-  });
-  return res.jsonp(output);
+server.get("/api/budgets/transactions", async (req, res) => {
+  try {
+    const output = db.budgets.map((budget) => {
+      const transactions = db.transactions
+        .filter((el) => el.category === budget.category && el.amount < 0)
+        .slice(0, 3);
+      return {
+        ...budget,
+        spent: 0,
+        latest: transactions,
+      };
+    });
+    res.jsonp(output);
+  } catch (error) {
+    return res.jsonp({ msg: "Server error" });
+  }
 });
 
 server.use(middlewares);
+server.use(cors({ origin: "*" }));
 server.use("/api", auth);
 server.use("/api", router);
 
