f329ab224bdea802f27c34f7f74ce8f6d50b4073
K-ost <golubkovru@gmail.com>
2025-03-05 21:44:06 +0200

    removing tests temporarily

--- src/tests/requestController.test.ts
+++ src/tests/requestController.test.ts
@@ -1,7 +1,3 @@
-import request from "supertest";
-
 describe("Request Controller", () => {
-  it("GET /transactions should return a list of transactions", async () => {
-    console.log("Test");
-  });
+  it("GET /transactions should return a list of transactions", async () => {});
 });
