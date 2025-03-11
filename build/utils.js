97a0575726371e2d9390a962b5ee4e0df8e8e924
Kostya <31858916+K-ost@users.noreply.github.com>
2025-03-11 20:53:48 +0200

    Delete build directory

--- build/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getToken = void 0;
-const getToken = (req) => {
-    const authHeader = req.header("Authorization");
-    const token = authHeader && authHeader.split(" ")[1];
-    return token;
-};
-exports.getToken = getToken;
