f1be1ae2e961b00abbf6d81eda6d09fe11c40e4f
K-ost <golubkovru@gmail.com>
2026-03-13 20:24:48 +0200

    removing unnecessary libs and token

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from "express";
-
-export const getToken = (req: Request): string => {
-  const authHeader = req.header("Authorization");
-  const token = authHeader && authHeader.split(" ")[1];
-  return token!;
-};
