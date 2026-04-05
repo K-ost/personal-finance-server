c69a96fb25b523fb3d7bc242a0db034cfed1905a
K-ost <golubkovru@gmail.com>
2026-04-05 19:52:11 +0300

    cookie-parser and cors policy

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter";
 import transactionRouter from "./routes/transactionRouter";
 import budgetsRouter from "./routes/budgetsRouter";
@@ -14,7 +15,12 @@ const server: Express = express();
 const PORT = process.env.PORT || 8000;
 server.set("json spaces", 2);
 server.use(bodyParser.json());
-server.use(cors());
+server.use(
+  cors({
+    origin: "https://personal-finance-server.onrender.com/",
+  }),
+);
+server.use(cookieParser());
 
 // Routes
 server.use("/api", transactionRouter);
